190e65a930113e3b4fbf8da7ebd8e5959aab1097
paololais <paolo.lais1@gmail.com>
2023-06-21 19:13:24 +0200

    home fragment, date picker

--- app/src/main/java/com/example/loginapp/HomeFragment.java
+++ app/src/main/java/com/example/loginapp/HomeFragment.java
@@ -1,23 +1,86 @@
 package com.example.loginapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
+    private Button btnSelectDate;
+    private TextView tvSelectDate;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnSelectDate = view.findViewById(R.id.btnSelectDate);
+        tvSelectDate = view.findViewById(R.id.tvSelectDate);
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        btnSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar selectedDate = Calendar.getInstance();
+                        selectedDate.set(year, month, dayOfMonth);
+
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE d MMMM", Locale.getDefault());
+                        String formattedDate = dateFormat.format(selectedDate.getTime());
+
+                        tvSelectDate.setText(formattedDate);
+
+                    }
+                },year, month,day);
+                dialog.show();
+
+            }
+        });
+
+        tvSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar selectedDate = Calendar.getInstance();
+                        selectedDate.set(year, month, dayOfMonth);
+
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE d MMMM", Locale.getDefault());
+                        String formattedDate = dateFormat.format(selectedDate.getTime());
+
+                        tvSelectDate.setText(formattedDate);
+
+                    }
+                },year, month,day);
+                dialog.show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/loginapp/MapsFragment.java
+++ app/src/main/java/com/example/loginapp/MapsFragment.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,32 +15,21 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends Fragment {
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    private OnMapReadyCallback callback;
+    private boolean isMapInitialized = false;
+    private GoogleMap googleMap;
+    private SharedPreferences sharedPreferences;
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            //start position
-            LatLng genoa = new LatLng(44.414165, 8.942184);
-            googleMap.addMarker(new MarkerOptions().position(genoa).title("Marker in Genoa"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(genoa));
-            googleMap.setMinZoomPreference(12.0f);
-            googleMap.setMaxZoomPreference(20.0f);
-        }
-    };
+    private static final String PREF_NAME = "MapPrefs";
+    private static final String PREF_CAMERA_LAT = "CameraLat";
+    private static final String PREF_CAMERA_LNG = "CameraLng";
+    private static final String PREF_CAMERA_ZOOM = "CameraZoom";
 
     @Nullable
     @Override
@@ -51,10 +42,81 @@ public class MapsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        sharedPreferences = requireContext().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+
+        callback = new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                googleMap = map;
+                if (isMapInitialized) {
+                    // Ripristina la vista del fragment di Google Maps
+                    // Esegui qui le operazioni necessarie per ripristinare la vista precedente
+
+                    // Ripristina la posizione della camera
+                    double lat = sharedPreferences.getFloat(PREF_CAMERA_LAT, 0f);
+                    double lng = sharedPreferences.getFloat(PREF_CAMERA_LNG, 0f);
+                    float zoom = sharedPreferences.getFloat(PREF_CAMERA_ZOOM, 0f);
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(new LatLng(lat, lng))
+                            .zoom(zoom)
+                            .build();
+
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                } else {
+                    // La vista non è stata inizializzata in precedenza
+                    // Esegui le operazioni di inizializzazione normale
+                    LatLng genoa = new LatLng(44.414165, 8.942184);
+                    googleMap.addMarker(new MarkerOptions().position(genoa).title("Marker in Genoa"));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(genoa));
+                    googleMap.setMinZoomPreference(12.0f);
+                    googleMap.setMaxZoomPreference(20.0f);
+
+                    isMapInitialized = true; // Imposta la vista come inizializzata
+                }
+            }
+        };
+
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (googleMap != null) {
+            // Salva la posizione della camera nelle SharedPreferences
+            CameraPosition cameraPosition = googleMap.getCameraPosition();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putFloat(PREF_CAMERA_LAT, (float) cameraPosition.target.latitude);
+            editor.putFloat(PREF_CAMERA_LNG, (float) cameraPosition.target.longitude);
+            editor.putFloat(PREF_CAMERA_ZOOM, cameraPosition.zoom);
+            editor.apply();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (googleMap != null && isMapInitialized) {
+            // Ripristina la vista del fragment di Google Maps
+            // Esegui qui le operazioni necessarie per ripristinare la vista precedente
+
+            // Ripristina la posizione della camera
+            double lat = sharedPreferences.getFloat(PREF_CAMERA_LAT, 0f);
+            double lng = sharedPreferences.getFloat(PREF_CAMERA_LNG, 0f);
+            float zoom = sharedPreferences.getFloat(PREF_CAMERA_ZOOM, 0f);
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(lat, lng))
+                    .zoom(zoom)
+                    .build();
+
+            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         }
     }
 }
