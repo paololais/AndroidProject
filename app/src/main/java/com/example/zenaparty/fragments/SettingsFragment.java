274799dce12187d7ce13c4ea1f196f75ae64fe73
LorenzoFabbricotti <lory.fabbricotti@gmail.com>
2023-07-18 15:57:51 +0200

    pupupu

--- app/src/main/java/com/example/zenaparty/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/zenaparty/fragments/SettingsFragment.java
@@ -1,18 +1,31 @@
 package com.example.zenaparty.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.zenaparty.R;
+import com.example.zenaparty.activities.LogActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class SettingsFragment extends Fragment {
 
+    Button logoutButton;
+
+    FirebaseUser user;
+
+    FirebaseAuth auth;
 
 
 
@@ -22,5 +35,25 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        logoutButton = view.findViewById(R.id.logoutButton);
+        user = auth.getCurrentUser();
+
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), LogActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
     }
 }
