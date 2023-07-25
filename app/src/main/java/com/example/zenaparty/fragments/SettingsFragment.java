e3624abc01e909f3df0ae77793dacac2cbb641c6
paololais <paolo.lais1@gmail.com>
2023-07-25 11:13:08 +0200

    last fixes

--- app/src/main/java/com/example/zenaparty/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/zenaparty/fragments/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.zenaparty.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -24,7 +27,9 @@ public class SettingsFragment extends Fragment {
 
     RelativeLayout logoutButton;
     RelativeLayout modificaProf;
-
+    SwitchCompat notificheSwitch;
+    private SharedPreferences sharedPreferences;
+    boolean notificheBool = true;
     FirebaseUser user;
 
     FirebaseAuth auth;
@@ -40,9 +45,15 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        sharedPreferences = requireActivity().getSharedPreferences("SavedValues", Context.MODE_PRIVATE);
+
         auth = FirebaseAuth.getInstance();
         logoutButton = view.findViewById(R.id.logoutButton);
+
         user = auth.getCurrentUser();
+
+        notificheSwitch = view.findViewById(R.id.notificationBool);
+        notificheBool = notificheSwitch.isChecked();
 
         ImageView goBack = view.findViewById(R.id.goBackBtn);
         goBack.setOnClickListener(view1 -> requireActivity().onBackPressed());
@@ -62,6 +73,14 @@ public class SettingsFragment extends Fragment {
             Intent intent = new Intent(getActivity(), LogActivity.class);
             startActivity(intent);
             requireActivity().finish();
+        });
+
+        notificheSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("Notifiche", notificheBool);
+            }
         });
     }
 }
